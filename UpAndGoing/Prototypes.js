2a7a07c829569029d574e705f1bfdd658762c68a
Jonathan Ody <jonathanody@users.noreply.github.com>
2016-04-23 08:23:35 +0100

    Prototype property example

--- UpAndGoing/Prototypes.js
+++ UpAndGoing/Prototypes.js
@@ -10,3 +10,31 @@ bar.b = "hello world";
 
 console.log(bar.b); // "hello world"
 console.log(bar.a); //  42 <-- delgated to 'foo'
+
+
+// prototype property
+Array.prototype.where = function (predicate) {
+    var matched = [];
+    
+    for(var i = 0; i < this.length; i++) {
+        if (predicate(this[i])) {
+            matched.push(this[i]);
+        }
+    }
+    
+    return matched
+}
+
+var greaterThanTwo = function (valueToTest) {
+    return valueToTest > 2;
+}
+
+var testArray = [1, 2, 3, 4, 5];
+var results = testArray.where(greaterThanTwo);
+outputArray(results);
+
+function outputArray(arrayToOutput) {
+    for(var i = 0; i < arrayToOutput.length; i++) {
+        console.log(arrayToOutput[i]);
+    }
+}
